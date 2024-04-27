52e034cac98913d3688673c98dae4c15a2aaccaf
Samet Gürhan <154097096+SametGurhan@users.noreply.github.com>
2024-04-28 02:16:40 +0300

    Screen PhotoSaver_rush
    Windows Forms kullanılarak ekrana rastgele resim aktarımı
    (resim boyutlarına göre ekranda taşma olabiliyor .Yaklaşık 200px_200 px ideal boyutlar.)

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace ScreenSaver
+{
+    partial class formScSaver
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formScSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "formScSaver";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "ScreenSaver";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.formScSaver_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.formScSaver_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.formScSaver_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.formScSaver_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class formScSaver : Form
+    {
+
+        List<Image> kediImages = new List<Image>();
+        List<kalpPict> kediPictures = new List<kalpPict>();
+        Random rnd = new Random();
+
+
+
+        class kalpPict
+        {
+            public int PictNum;
+            public float X;
+            public float Y;
+            public float Speed;
+        }
+        public formScSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void formScSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+
+        private void formScSaver_Load(object sender, EventArgs e)
+        {
+            string[] images = System.IO.Directory.GetFiles("fotos");
+            foreach(string item in images)
+            {
+                kediImages.Add(new Bitmap(item));
+
+            }
+            for (int i=0; i<2000; i++)
+            {
+                kalpPict mp = new kalpPict();
+                mp.PictNum = i%kediImages.Count;
+                mp.X = rnd.Next(0,Width);
+                mp.Y = rnd.Next(0, Height);
+                kediPictures.Add(mp);
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+         
+        }
+
+        private void formScSaver_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (kalpPict kp in kediPictures)
+            {
+                e.Graphics.DrawImage(kediImages[kp.PictNum],kp.X,kp.Y);
+                kp.X -=2;
+                if (kp.X<-250)
+                {
+                    kp.X = Width + rnd.Next(20, 100);
+
+                }
+            }
+        }
+
+        private void formScSaver_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+    }
+}
